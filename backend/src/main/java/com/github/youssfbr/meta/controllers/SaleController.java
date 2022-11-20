c9c09f707bc7f5e0e1af42c1ee99a678f265218f
Alisson Youssf <youssfbr@gmail.com>
2022-11-19 22:10:16 -0300

    SMS

--- backend/src/main/java/com/github/youssfbr/meta/controllers/SaleController.java
+++ backend/src/main/java/com/github/youssfbr/meta/controllers/SaleController.java
@@ -2,13 +2,11 @@ package com.github.youssfbr.meta.controllers;
 
 import com.github.youssfbr.meta.entities.Sale;
 import com.github.youssfbr.meta.services.SaleService;
+import com.github.youssfbr.meta.services.SmsService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
@@ -17,12 +15,19 @@ public class SaleController {
 
     private final SaleService saleService;
 
+    private final SmsService smsService;
+
     @GetMapping
     public Page<Sale> pageSales(
             @RequestParam(value="minDate", defaultValue = "") String minDate,
             @RequestParam(value="maxDate", defaultValue = "") String maxDate,
             Pageable pageable) {
         return saleService.pageSales(minDate, maxDate, pageable);
+    }
+
+    @GetMapping("/{id}/notification")
+    public void notifySms(@PathVariable Long id) {
+        smsService.sendSms(id);
     }
 
 }
