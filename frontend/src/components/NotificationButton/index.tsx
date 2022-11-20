853afed29a716886e6b54e66d91d63cda2422e44
Alisson Youssf <youssfbr@gmail.com>
2022-11-20 17:46:32 -0300

    Toast

--- frontend/src/components/NotificationButton/index.tsx
+++ frontend/src/components/NotificationButton/index.tsx
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import { toast } from 'react-toastify';
 import icon from '../../assets/img/notification-icon.svg';
 import { BASE_URL } from '../../utils/request';
 import './styles.css';
@@ -10,7 +11,7 @@ type Props = {
 function handleClick(saleId: number): void {
   axios(`${BASE_URL}/api/v1/sales/${saleId}/notification`)
   .then(response => {
-    console.log("sucesso");    
+    toast.info("SMS enviado.") ;
   });
 }
 
